0d96d296a0051893a53f0028844d07789709930a
BeatrizMioranza <beatrizmioranza2508@gmail.com>
2022-05-01 16:13:15 -0300

    Terminei o JS da Cifra de César

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+
+// Botão decodificar e verificador de Base || César
 var decodificar = document.querySelector("#decodificar")
-decodificar.addEventListener("click", function(){
-    var input= document.querySelector("#decodificar").value;
+decodificar.addEventListener("click", function () {
+    var input = document.querySelector("#decodificar").value;
     var btn = document.querySelector("#btn")
     var remocao = document.querySelector('button')
-    if(remocao){
+    if (remocao) {
         remocao.remove()
     }
     var botao = document.createElement('button')
@@ -12,25 +14,26 @@ decodificar.addEventListener("click", function(){
     botao.innerText = `Decodificar Mensagem!`
     var prevent = document.querySelector('#idbotao')
     prevent.addEventListener("click", (e) => {
-    e.preventDefault();
-    var seletor = document.querySelector('#seletor').value
-    var mensagem = document.querySelector("#msg").value
-    if(seletor == "base" && mensagem != ""){
-         return decodificadorBase64()
-    } else if(seletor == "cesar"){
-        return 
-    } else {
-        alert ("Escreva sua mensagem")
-    }
- })
+        e.preventDefault();
+        var seletor = document.querySelector('#seletor').value
+        var mensagem = document.querySelector("#msg").value
+        if (seletor == "base" && mensagem != "") {
+            return decodificadorBase64()
+        } else if (seletor == "cesar") {
+            return descodificarCesar()
+        } else {
+            alert("Escreva sua mensagem")
+        }
+    })
 });
 
+// Botão codificar e verificador de Base || César
 var codificar = document.querySelector("#codificar")
-    codificar.addEventListener("click", function(){
-    var input= document.querySelector("#codificar").value;
+codificar.addEventListener("click", function () {
+    var input = document.querySelector("#codificar").value;
     var btn = document.querySelector("#btn")
     var remocao = document.querySelector('button')
-    if(remocao){
+    if (remocao) {
         remocao.remove()
     }
     var botao = document.createElement('button')
@@ -39,18 +42,20 @@ var codificar = document.querySelector("#codificar")
     botao.innerText = `Codificar Mensagem!`
     var prevent = document.querySelector('#idbotao')
     prevent.addEventListener("click", (e) => {
-    e.preventDefault();
-    var seletor = document.querySelector('#seletor').value
-    var mensagem = document.querySelector("#msg").value
-    if(seletor == "base" && mensagem != ""){
-         return codificadorBase64()
-    } else if(seletor == "cesar"){
-        return 
-    } else {
-        alert ("Escreva sua mensagem")
-    }
- })
+        e.preventDefault();
+        var seletor = document.querySelector('#seletor').value
+        var mensagem = document.querySelector("#msg").value
+        if (seletor == "base" && mensagem != "") {
+            return codificadorBase64()
+        } else if (seletor == "cesar") {
+            return codificarCesar()
+                  
+        } else {
+            alert("Escreva sua mensagem")
+        }
+    })
 });
+// Base 64 
 function codificadorBase64() {
     var mensagem = document.querySelector("#msg").value
     var resultado = document.querySelector("#resultado")
@@ -62,3 +67,36 @@ function decodificadorBase64() {
     var resultado = document.querySelector("#resultado")
     resultado.innerText = atob(mensagem);
 };
+
+// Cifra de César
+function codificarCesar(){ 
+    var deslocamento = document.querySelector("#deslocamento").value
+    var resultado = document.querySelector("#resultado")
+    var mensagem = document.querySelector("#msg").value
+    var msgCodigo= ""
+    var teste = parseInt(deslocamento)
+    for (var i = 0; i < mensagem.length; i++) {
+    var codAscii = mensagem[i].charCodeAt();
+    if(codAscii >= 65 && codAscii <= 90){
+       var operacao = (codAscii - 65 + teste) % 26
+     msgCodigo += String.fromCharCode(operacao + 65);
+    }
+}
+    resultado.innerText = msgCodigo
+
+}
+function descodificarCesar(){
+    var deslocamento = document.querySelector("#deslocamento").value
+    var resultado = document.querySelector("#resultado")
+    var mensagem = document.querySelector("#msg").value
+    var msgCodigo= ""
+    var teste = parseInt(deslocamento)
+    for (var i = 0; i < mensagem.length; i++) {
+    var codAscii = mensagem[i].charCodeAt();
+    if(codAscii >= 65 && codAscii <= 90){
+       var operacao = (codAscii - 65 - teste + 26) % 26
+     msgCodigo += String.fromCharCode(operacao + 65);
+    }
+    resultado.innerText= msgCodigo
+}
+}
